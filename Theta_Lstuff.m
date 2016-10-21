41f0ad92adc9d74106aa70083eaeb06e06dd08df
Claudia <ckann10@gmail.com>
2016-10-21 09:24:49 -0500

    working.....

--- Theta_Lstuff.m
+++ Theta_Lstuff.m
@@ -1,13 +1,33 @@
-theta_L = linspace(-pi/4,pi/4,1000);
-theta_c = linspace(-pi/4,pi/4,1000);
+theta_L = linspace(-pi/2,pi/2,100);
+theta_c = linspace(-pi/2,pi/2,100);
 g = 9.81;
 k1 = g;
-k2 = linspace(0,100,1000);
+k2 = linspace(0,1000,100);
 m = 1;
 l = 1/3;
 
+amin = 99999;
+for i = 1:100
+    for k = 1:100
+        for j = 1:100
+            Tm(j) = k2(i)*(theta_L(j)-theta_c(k))+cos(theta_L(j))*m*g*l;
+        end
+        a = abs(trapz(theta_L, Tm));
+        if a <= amin
+            amin = a;
+            thc = theta_c(k);
+            k2g = k2(i);
+        else
+            amin = amin;
+        end
+    end
+end
 
-for i = 1:1000
+
+theta_c = thc;
+k2 = k2g;
+
+for i = 1:100
     TL(i) = cos(theta_L(i))*m*g*l;
     Tk(i) = k2*(theta_L(i)-theta_c);
     Tm(i) = Tk(i) + TL(i);
@@ -15,3 +35,7 @@ end
 
 plot(theta_L,TL,'g',theta_L,Tm,'b', theta_L,Tk,'k') 
 
+
+
+            
+            
