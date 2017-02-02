a9e224a5f1127822310185858ae19ca8989c588b
Preston <plh1@rice.edu>
2017-02-01 21:21:16 -0600

    so the testing code sort of works, but it relies on pauses for ten seconds to allow variables to be written.

--- simulink/multi_pass.m
+++ simulink/multi_pass.m
@@ -10,12 +10,18 @@ controller = 1;
 plant = 1;
 k_s_max = 124;
 k_s_min = k_s_max/3;
+test = 0;
 for i = linspace(k_s_min,k_s_max,10)
     for j = 1:4
         controller = j;
         k_s = i;
         load_system('gui1');
         set_param('gui1','SimulationCommand','start')
-        Simulink.sdi.view
+        pause(10);
+        test = test + 1;
+        TestData.theta_m{test} = theta_m.signals.values;
+        TestData.theta_L{test} = theta_m.signals.values;
+        TestData.commanded_torque{test} = commanded_torque.signals.values;
+        TestData.torque_RMS{test} = torque_RMS.signals.values;
     end
 end
